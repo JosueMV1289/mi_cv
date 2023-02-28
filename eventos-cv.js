95b7c8cc8a0b03b0120f650a3d2440acb7ff062f
JosueMV1289 <125925168+JosueMV1289@users.noreply.github.com>
2023-02-28 17:10:11 -0300

    Add files via upload

---
+++ eventos-cv.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById("boton_ocultar").addEventListener("click", function() {
+    document.getElementById("lista").style.display='none';
+});
+
+document.getElementById("boton_mostrar").addEventListener("click", function() {
+    document.getElementById("lista").style.display='';
+});
+
+document.getElementById("boton_ocultar_2").addEventListener("click", function() {
+    document.getElementById("lista2").style.display='none';
+});
+
+document.getElementById("boton_mostrar_2").addEventListener("click", function() {
+    document.getElementById("lista2").style.display='';
+});
